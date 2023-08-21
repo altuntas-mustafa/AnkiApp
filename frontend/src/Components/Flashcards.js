c383767231ce0e12e657ee005e9c3da4e93a3f23
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2023-08-21 11:55:47 +0200

    some bug fixed

--- frontend/src/Components/Flashcards.js
+++ frontend/src/Components/Flashcards.js
@@ -1,45 +1,73 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, Link, useHistory } from "react-router-dom";
 import "./FlashCards.css";
 
 const Flashcards = () => {
   const { deckName } = useParams();
-  const [flashcards, setFlashcards] = useState([]);
-  const [currentCardIndex, setCurrentCardIndex] = useState(0);
-  const [isFlipped, setIsFlipped] = useState(false); // Initialize with front side visible
-
   const formattedDeckName = deckName.toLowerCase().replace(/\s+/g, "-");
 
-  useEffect(() => {
-    axios
-      .get(`https://anki-app-exau.vercel.app/api/deck/${formattedDeckName}`)
-      .then((response) => {
-        const data = response.data;
-        setFlashcards(data);
-        setCurrentCardIndex(0); // Reset current card index
-        setIsFlipped(false); // Ensure front side is visible when a new deck is loaded
-      })
-      .catch((error) => console.error("Error fetching flashcards:", error));
-  }, [formattedDeckName]);
+  const [shuffledFlashcards, setShuffledFlashcards] = useState([]);
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [completed, setCompleted] = useState(false);
+  const [redirected, setRedirected] = useState(false);
+
+  const shuffleArray = (array) => {
+    return array.sort(() => Math.random() - 0.5);
+  };
 
   const handleFlip = () => {
     setIsFlipped(!isFlipped);
   };
 
   const handleNextCard = () => {
-    setCurrentCardIndex((prevIndex) => (prevIndex + 1) % flashcards.length);
-    setIsFlipped(false); // Reset to front side when moving to the next card
+    if (currentCardIndex < shuffledFlashcards.length - 1) {
+      setCurrentCardIndex(currentCardIndex + 1);
+      setIsFlipped(false);
+    } else {
+      setCompleted(true);
+    }
   };
 
-  if (flashcards.length === 0) {
+  useEffect(() => {
+    axios
+      .get(`https://anki-app-exau.vercel.app/api/deck/${formattedDeckName}`)
+      .then((response) => {
+        const data = response.data;
+        setShuffledFlashcards(data); // Set flashcards without shuffling initially
+        setCurrentCardIndex(0);
+        setIsFlipped(false);
+        setCompleted(false);
+      })
+      .catch((error) => console.error("Error fetching flashcards:", error));
+  }, [formattedDeckName]);
+
+  useEffect(() => {
+    if (completed && !redirected) {
+      const timeout = setTimeout(() => {
+        setRedirected(true);
+        window.location.href = "/decks"; // Redirect using window.location
+      }, 1200); // 1200 milliseconds = 1.2 seconds
+
+      return () => clearTimeout(timeout); // Clean up on component unmount
+    }
+  }, [completed, redirected]);
+
+  if (shuffledFlashcards.length === 0) {
     return <div>Loading...</div>;
   }
 
-  const currentFlashcard = flashcards[currentCardIndex];
+  const currentFlashcard = shuffledFlashcards[currentCardIndex];
+  const isLastFlashcard = currentCardIndex === shuffledFlashcards.length - 1;
 
   return (
     <div className="flashcards-container">
+      {completed && !redirected && (
+        <div>
+          <p>Congratulations! You've completed all flashcards.</p>
+        </div>
+      )}
       <h2>{deckName}</h2>
       <div className={`flashcard ${isFlipped ? "flipped" : ""}`}>
         <div className="flashcard-content front">
@@ -49,9 +77,16 @@ const Flashcards = () => {
         {isFlipped && (
           <div className="flashcard-content back">
             <p className="front-word">{currentFlashcard.front}</p>
-            <hr  className="line"/>
+            <hr className="line" />
             <p className="back-word">{currentFlashcard.back}</p>
-            <button onClick={handleNextCard}>Next Card</button>
+            {isLastFlashcard && (
+              <Link to="/decks" className="viewDeck">
+                Go to Decks
+              </Link>
+            )}
+            {!isLastFlashcard && (
+              <button onClick={handleNextCard}>Next Card</button>
+            )}
           </div>
         )}
       </div>
