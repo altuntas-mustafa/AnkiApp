7ad753c2cb5e50a4db955277415eb05fa3d84a53
Mustafa Altuntas <93078239+altuntas-mustafa@users.noreply.github.com>
2023-08-24 14:41:51 +0200

    create card is changed

--- src/Components/JS/AddDeck.js
+++ src/Components/JS/AddDeck.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { collection, addDoc, doc, setDoc, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
 import "../CSS/AddDeck.css";
-// http://localhost:3001/api/create-deck
 
 const AddDeck = () => {
   const [deckInfo, setDeckInfo] = useState({
@@ -12,127 +13,179 @@ const AddDeck = () => {
     ],
   });
 
+  const [selectedOption, setSelectedOption] = useState("manual"); // Default option is manual
+  const [uploadedFile, setUploadedFile] = useState(null); // Track uploaded file
+
   const handleCardChange = (index, field, value) => {
     const newCards = [...deckInfo.cards];
     newCards[index][field] = value;
     setDeckInfo({ ...deckInfo, cards: newCards });
   };
 
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const jsonData = JSON.parse(event.target.result);
+      setDeckInfo({
+        deckName: jsonData.deckName || "",
+        language: jsonData.language || "",
+        cards: jsonData.cards || [],
+      });
+      setUploadedFile(file); // Set the uploaded file here
+      setSelectedOption("manual"); // Switch to manual input
+    };
+    reader.readAsText(file);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Check if required fields are empty
     if (!deckInfo.deckName || !deckInfo.language) {
       alert("Please fill out the deck name and language fields.");
       return;
     }
 
-    // Check if each card has at least 2 total words (combined front and back)
     if (
       deckInfo.cards.some(
         (card) =>
           (card.front.trim() + " " + card.back.trim()).split(" ").length < 2
       )
     ) {
-      console.log(
-        "Each card should have at least 2 total words (front and back combined)."
-      );
-      alert(
-        "Each card should have at least 2 total words (front and back combined)."
-      );
+      alert("Each card should have at least 2 total words (front and back combined).");
+      return;
+    }
+
+    const deckQuery = query(collection(db, "languages", deckInfo.language, "decks"), where("name", "==", deckInfo.deckName));
+    const deckQuerySnapshot = await getDocs(deckQuery);
+
+    if (!deckQuerySnapshot.empty) {
+      alert("A deck with the same name already exists. Please choose a different deck name.");
       return;
     }
 
     try {
-      const response = await fetch("https://ankiappclone-git-main-mustafa-altuntas.vercel.app/api/create-deck", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(deckInfo),
+      const deckDocRef = doc(db, "languages", deckInfo.language, "decks", deckInfo.deckName);
+      await setDoc(deckDocRef, {
+        name: deckInfo.deckName,
       });
 
-      if (response.ok) {
-        const responseData = await response.json();
-        console.log(responseData.message); // "Deck and cards created successfully"
-        setDeckInfo({
-          deckName: "",
-          language: "",
-          cards: [
-            { front: "", back: "" },
-            { front: "", back: "" },
-          ],
+      const flashcardsCollectionRef = collection(deckDocRef, "flashcards");
+      deckInfo.cards.forEach(async (card) => {
+        await addDoc(flashcardsCollectionRef, {
+          front: card.front,
+          back: card.back,
         });
-      } else {
-        const errorData = await response.json();
-        console.log(errorData.error); // Display the error message sent from the server
-      }
+      });
+
+      console.log("Deck and cards created successfully");
+      
+      setDeckInfo({
+        deckName: "",
+        language: "",
+        cards: [
+          { front: "", back: "" },
+          { front: "", back: "" },
+        ],
+      });
     } catch (error) {
-      console.error("Error:", error);
+      console.error("Error creating deck:", error);
     }
   };
 
   return (
     <div className="add-deck-container">
       <h2 className="form-heading">Create New Deck</h2>
+      <nav className="option-navbar">
+        <div
+          className={`option ${selectedOption === "manual" ? "selected" : ""}`}
+          onClick={() => setSelectedOption("manual")}
+        >
+          Manual Input
+        </div>
+        <div
+          className={`option ${selectedOption === "upload" ? "selected" : ""}`}
+          onClick={() => setSelectedOption("upload")}
+        >
+          Upload JSON File
+        </div>
+      </nav>
       <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Deck Name:</label>
-          <input
-            type="text"
-            value={deckInfo.deckName}
-            onChange={(e) =>
-              setDeckInfo({ ...deckInfo, deckName: e.target.value })
-            }
-          />
-        </div>
-        <div className="form-group">
-          <label> Language :</label>
-          <input
-            type="text"
-            value={deckInfo.language}
-            onChange={(e) =>
-              setDeckInfo({ ...deckInfo, language: e.target.value })
-            }
-          />
-        </div>
-        <div className="card-group">
-          {deckInfo.cards.map((card, index) => (
-            <div key={index} className="input-group">
-              <label>Front:</label>
+        {selectedOption === "upload" && (
+          <div className="form-group">
+            <label>Upload JSON File:</label>
+            <input
+              type="file"
+              accept=".json"
+              onChange={handleFileUpload}
+            />
+          </div>
+        )}
+        {selectedOption === "manual" || uploadedFile ? ( // Render manual input form if selected or file uploaded
+          <div>
+            <div className="form-group">
+              <label>Deck Name:</label>
               <input
                 type="text"
-                value={card.front}
+                value={deckInfo.deckName}
                 onChange={(e) =>
-                  handleCardChange(index, "front", e.target.value)
-                }
-              />
-              <label>Back:</label>
-              <input
-                type="text"
-                value={card.back}
-                onChange={(e) =>
-                  handleCardChange(index, "back", e.target.value)
+                  setDeckInfo({ ...deckInfo, deckName: e.target.value })
                 }
               />
             </div>
-          ))}
-        </div>
-        <button
-          type="button"
-          className="add-card-button"
-          onClick={() =>
-            setDeckInfo({
-              ...deckInfo,
-              cards: [...deckInfo.cards, { front: "", back: "" }],
-            })
-          }
-        >
-          Add Card
-        </button>
-        <button type="submit" className="create-deck-button">
-          Create Deck
-        </button>
+            <div className="form-group">
+              <label> Language :</label>
+              <input
+                type="text"
+                value={deckInfo.language}
+                onChange={(e) =>
+                  setDeckInfo({ ...deckInfo, language: e.target.value })
+                }
+              />
+            </div>
+            <div className="card-group">
+              {deckInfo.cards.map((card, index) => (
+                <div key={index} className="input-group">
+                  <label>Front:</label>
+                  <input
+                    type="text"
+                    value={card.front}
+                    onChange={(e) =>
+                      handleCardChange(index, "front", e.target.value)
+                    }
+                  />
+                  <label>Back:</label>
+                  <input
+                    type="text"
+                    value={card.back}
+                    onChange={(e) =>
+                      handleCardChange(index, "back", e.target.value)
+                    }
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="button-container">
+              <button
+                type="button"
+                className="add-card-button"
+                onClick={() =>
+                  setDeckInfo({
+                    ...deckInfo,
+                    cards: [...deckInfo.cards, { front: "", back: "" }],
+                  })
+                }
+              >
+                Add Card
+              </button>
+              <button type="submit" className="create-deck-button">
+                Create Deck
+              </button>
+            </div>
+          </div>
+        ) : null}
       </form>
     </div>
   );
